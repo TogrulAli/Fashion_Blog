1db57e26e5b4420cf07d9226b810630b3121f21a
TogrulAli <allahmanli@yahoo.com>
2022-11-23 18:14:14 +0100

    new

---
+++ script.js
@@ -0,0 +1,39 @@
+function openForm() {
+    document.getElementById("formPopup").style.display = "block";
+}
+function closeForm() {
+    document.getElementById("formPopup").style.display = "none";
+}
+
+
+
+
+
+
+function openCloseForm() {
+    let element = document.getElementById("popUpTitleMain")
+    element.classList.toggle("popUpTitleContainer")
+}
+function openCloseForm1() {
+    let element = document.getElementById("popUpTitleMain1")
+    element.classList.toggle("popUpTitleContainer1")
+}
+function openCloseForm2() {
+    let element = document.getElementById("popUpTitleMain2")
+    element.classList.toggle("popUpTitleContainer2")
+}
+
+
+
+// }
+
+// function openCloseForm() {
+//     let element = document.getElementById("popUpTitleMain");
+//     if (element.style.display === "flex") {
+//         element.style.display === "none"
+//     }
+//     else {
+//         element.style.display === "none"
+//     }
+// }
+
